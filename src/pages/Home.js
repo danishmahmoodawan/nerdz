58fb4daeed7d4e4b6d61f41298f80b955c90ac0f
danishmahmoodawan <dani.ciitwah@gmail.com>
2022-02-11 17:39:01 +0500

    New changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -42,6 +42,24 @@ function Home() {
         </div>
           
           </section>
+
+          <section class="sec_two">
+        <div class="container">
+        <div class="row">
+        <div class="col-6 col_set_">
+        <h1>Who Are We?</h1>
+        <p>Lorem ipsum dolor sit amet,<br /> consectetur adipiscing elit, sed  <br />do eiusmod tempor incididunt ut <br /> labore et dolore magna aliqua.<br /> Ut enim ad minim veniam, quis<br /> nostrud exercitation ullamco<br /> laboris nisi ut aliquip ex ea<br /> commodo consequat. Duis aute<br /> irure dolor in reprehenderit in<br /> voluptate velit esse cillum dolore<br /> eu fugiat nulla pariatur. <br />Excepteur sint occaecat cupidatat <br />non proident, sunt in <br />culpa qui officia deserunt mollit <br />anim id est laborum</p>
+      </div>
+      <div class="col-6 ">
+        <img src="assets/images/green_nrdz.png" alt="" />
+
+      </div>
+      </div>
+
+        </div>
+          
+          </section>
+          
 	</>
   );
 }
