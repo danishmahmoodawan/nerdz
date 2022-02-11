9c04fe24f18568630a4d7ce0528b1c44eb35b098
danishmahmoodawan <dani.ciitwah@gmail.com>
2022-02-11 20:59:25 +0500

    version 1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ function Home() {
     
   return (
    <>
- <div className="cotainer bg-nav" style={{paddingLeft: "10%",paddingRight: "10%"}}>
+ <div className="cotainer bg-nav fixed-top" style={{paddingLeft: "10%",paddingRight: "10%"}}>
             <nav className="navbar navbar-expand-lg navbar-light ">
                 <a className="navbar-brand" href="#"><img className="logo_s" src="assets/images/logo.png"/></a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
@@ -24,18 +24,18 @@ function Home() {
                             </a>
                         </li>
                         <li className="nav-item set_pading">
-                        <button class="btn btn-light button_join"  href="https://discord.gg/dDjBp8YF" target={{target:"_blank"}}>Join discord </button>
+                        <button className="btn btn-light button_join"  href="https://discord.gg/dDjBp8YF" target={{target:"_blank"}}>Join discord </button>
                             
                         </li>
                     </ul>
                       </div>
             </nav>
         </div>
-        <section class="sec_bg bg-image_sec">
-        <div class="container text-center">
-        <div class="row">
-        <div class="col-12 seting_row">
-        <button class="btn  button_nerd"  href="https://discord.gg/dDjBp8YF" target={{target:"_blank"}}>Join The Nerdz </button>
+        <section className="sec_bg bg-image_sec">
+        <div className="container text-center">
+        <div className="row">
+        <div className="col-12 seting_row">
+        <button className="btn  button_nerd"  href="https://discord.gg/dDjBp8YF" target={{target:"_blank"}}>Join The Nerdz </button>
       </div>
       </div>
 
@@ -43,14 +43,14 @@ function Home() {
           
           </section>
 
-          <section class="sec_two">
-        <div class="container">
-        <div class="row">
-        <div class="col-6 col_set_">
+          <section className="sec_two">
+        <div className="container">
+        <div className="row">
+        <div className="col-6 col_set_">
         <h1>Who Are We?</h1>
         <p>Lorem ipsum dolor sit amet,<br /> consectetur adipiscing elit, sed  <br />do eiusmod tempor incididunt ut <br /> labore et dolore magna aliqua.<br /> Ut enim ad minim veniam, quis<br /> nostrud exercitation ullamco<br /> laboris nisi ut aliquip ex ea<br /> commodo consequat. Duis aute<br /> irure dolor in reprehenderit in<br /> voluptate velit esse cillum dolore<br /> eu fugiat nulla pariatur. <br />Excepteur sint occaecat cupidatat <br />non proident, sunt in <br />culpa qui officia deserunt mollit <br />anim id est laborum</p>
       </div>
-      <div class="col-6 ">
+      <div className="col-6 ">
         <img src="assets/images/green_nrdz.png" alt="" />
 
       </div>
@@ -59,7 +59,212 @@ function Home() {
         </div>
           
           </section>
-          
+
+          <section className="sec_three bubble">
+        <div className="container">
+        <div className="row">
+        <div className="col-12 margins_se">
+<h1 className="set_head">WHY BE A NERD?</h1>
+</div>
+</div>
+<div className="row nerd_row text-center">
+<div className="col-3">
+<div className="bg_ser">
+<img src="assets/images/Rectangle99.png" alt="" /> </div>
+<span className="span_font">EDUCATION</span>
+
+</div>
+<div className="col-3">
+<div className="bg_ser">
+<img src="assets/images/Rectangle99.png" alt="" /> </div>
+<span className="span_font">GIVEAWAY</span>
+
+</div>
+<div className="col-3">
+<div className="bg_ser">
+<img src="assets/images/Rectangle99.png" alt="" /> </div>
+<span className="span_font">SUPPORT</span>
+
+</div>
+<div className="col-3">
+<div className="bg_ser">
+<img src="assets/images/Rectangle99.png" alt="" /> </div>
+<span className="span_font">WAY TO
+THE METAVERSE</span>
+
+</div>
+
+        </div> 
+        </div>
+</section>    
+
+
+          <section className="sec_five">
+        <div className="container cols_set_">
+        <div className="row">
+        <div className="col-12 margins_se">
+        <h1 class="headonese">TEAM</h1>
+        </div>
+        </div>
+        <div className="row">
+        <div className="col-4"> 
+        <div className="mian_taem">
+        <span class="post">Position</span>
+        <img src="assets/images/white_img.png" alt="" /> 
+        <h1 className="team_name"> John Doe </h1>
+        <div className="set_oicin"> <img className="d-inline set_imgg" src="assets/images/icon/ICON_TWITTER.svg"/><span className="d-inline">@johndoe</span></div>
+        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in</span>
+        </div>
+         </div>
+        <div className="col-4">
+        <div className="mian_taem team_bg_s">
+        <span class="post">Position</span>
+        <img src="assets/images/white_img.png" alt="" /> 
+        <h1 className="team_name"> John Doe </h1>
+        <div className="set_oicin"> <img className="d-inline set_imgg" src="assets/images/icon/ICON_TWITTER.svg"/><span className="d-inline">@johndoe</span></div>
+        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in</span>
+        </div>
+</div>
+        <div className="col-4"> <div className="mian_taem team_bg_r">
+        <span class="post">Position</span>
+        <img src="assets/images/white_img.png" alt="" /> 
+        <h1 className="team_name"> John Doe </h1>
+        <div className="set_oicin"> <img className="d-inline set_imgg" src="assets/images/icon/ICON_TWITTER.svg"/><span className="d-inline">@johndoe</span></div>
+        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in</span>
+        </div>
+</div>
+
+        </div>
+        <div className="row pt-3 pb-3 row_team_set">
+        <div className="col-4"> 
+        <div className="mian_taem team_bg_b">
+        <span class="post">Position</span>
+        <img src="assets/images/white_img.png" alt="" /> 
+        <h1 className="team_name"> John Doe </h1>
+        <div className="set_oicin"> <img className="d-inline set_imgg" src="assets/images/icon/ICON_TWITTER.svg"/><span className="d-inline">@johndoe</span></div>
+        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in</span>
+        </div>
+         </div>
+        <div className="col-4">
+        <div className="mian_taem team_bg_cv">
+        <span class="post">Position</span>
+        <img src="assets/images/white_img.png" alt="" /> 
+        <h1 className="team_name"> John Doe </h1>
+        <div className="set_oicin"> <img className="d-inline set_imgg" src="assets/images/icon/ICON_TWITTER.svg"/><span className="d-inline">@johndoe</span></div>
+        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in</span>
+        </div>
+</div>
+        <div className="col-4"> <div className="mian_taem team_bg_tr">
+        <span class="post">Position</span>
+        <img src="assets/images/white_img.png" alt="" /> 
+        <h1 className="team_name"> John Doe </h1>
+        <div className="set_oicin"> <img className="d-inline set_imgg" src="assets/images/icon/ICON_TWITTER.svg"/><span className="d-inline">@johndoe</span></div>
+        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in</span>
+        </div>
+</div>
+
+        </div> 
+        </div>
+</section>    
+
+          <section className="sec_faq">
+        <div className="container-fluid p-0">
+        <div className="row">
+        <div className="col-12 margins_se">
+        <h1 class="headonese faq">FAQ</h1>
+        </div>
+        </div>
+        <div className="container-fluid bg_rea p-0">
+  <div class="row mt-3 mb-3 back_bl">
+         <div class="col-md-12 col-sm-12">
+            <div id="accordion">
+               <div class="card">
+                  <div class="card-header" id="headingOne">
+                     <h5 class="mb-0">
+                        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        SOME QUESTION SOME TEXT SOME QUESTION ?
+                        <i class="fas fa-sort-down"></i>  </button>
+                     </h5>
+                  </div>
+                  <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                     <div class="card-body">
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum	
+                     </div>
+                  </div>
+               </div>
+               <div class="card">
+                  <div class="card-header" id="headingTwo">
+                     <h5 class="mb-0">
+                        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        SOME QUESTION SOME TEXT SOME QUESTION ?
+                        <i class="fas fa-sort-down"></i>  </button>
+                     </h5>
+                  </div>
+                  <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                     <div class="card-body">
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum                     </div>
+                  </div>
+               </div>
+               <div class="card">
+                  <div class="card-header" id="headingThree">
+                     <h5 class="mb-0">
+                        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        SOME QUESTION SOME TEXT SOME QUESTION ?
+                        <i class="fas fa-sort-down"></i>	  </button>
+                     </h5>
+                  </div>
+                  <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                     <div class="card-body">
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum                     </div>
+                  </div>
+               </div>
+               <div class="card">
+                  <div class="card-header" id="headingThre">
+                     <h5 class="mb-0">
+                        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThre" aria-expanded="false" aria-controls="collapseThree">
+                        SOME QUESTION SOME TEXT SOME QUESTION ?
+                        <i class="fas fa-sort-down"></i></button>
+                     </h5>
+                  </div>
+                  <div id="collapseThre" class="collapse" aria-labelledby="headingThre" data-parent="#accordion">
+                     <div class="card-body">
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum                     </div>
+                  </div>
+               </div>
+               <div class="card">
+                  <div class="card-header" id="headingThreew">
+                     <h5 class="mb-0">
+                        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThreew" aria-expanded="false" aria-controls="collapseThree">
+                        SOME QUESTION SOME TEXT SOME QUESTION ?
+                        <i class="fas fa-sort-down"></i>	</button>
+                     </h5>
+                  </div>
+                  <div id="collapseThreew" class="collapse" aria-labelledby="headingThreew" data-parent="#accordion">
+                     <div class="card-body">
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+         <div class="row row text-center">
+  <div className="col-12 setings_row pb-5">
+        <button className="btn  button_nerds"  href="https://discord.gg/dDjBp8YF" target={{target:"_blank"}}>Join The Nerdz </button>
+      </div>
+  </div>
+        </div>
+     </div>
+        </section>
+<section className="footer">
+  <div class="container ">
+  <div class="row seting_row">
+  <div className="col-12  text-center">
+  <img className="seicon" src="assets/images/footer-logo.png"/>
+
+      </div>
+  </div>
+  </div>
+  </section>
 	</>
   );
 }
